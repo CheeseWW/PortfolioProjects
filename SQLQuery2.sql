6c467450e4da9a0b13d56d8ac7e352689c102a7d
CheeseWW <102394191+CheeseWW@users.noreply.github.com>
2022-04-02 22:24:51 -0700

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,7 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [EmployeeID]
-      ,[FirstName]
-      ,[LastName]
-      ,[Age]
-      ,[Gender]
-  FROM [SQL Tut 1]  .[dbo].[EmployeeDemographics]
